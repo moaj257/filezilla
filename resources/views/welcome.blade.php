b921fc2c1d96d4398a49fdc837821a583c1e7829
peppermint <63215668+ppmint1@users.noreply.github.com>
2023-05-22 21:21:42 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,12 @@
   </head>
   <body>
     <h1>Laravel 10 Test App</h1>
-    <h5>By <a href="https://github.com/ppmint1" target="_blank">@ppmint1</a>, intended to be used as infrastructure testing.</h5>
-    <h5>Source code publicly available here at  <a href="https://github.com/ppmint1/laravel-test" target="_blank"> https://github.com/ppmint1/laravel-test</a>, intended to be used as infrastructure testing.</h5>
-    <p>Server IP : <string>{{ request()->server('SERVER_ADDR') }}</strong></p>
-    <p>Your IP : <string>{{ request()->ip() }}</strong><br>Your IP address may be different from the one used by your current device if you are using a VPN, Proxy, behind NAT, or your ISP uses CGNAT (Carrier-grade NAT). </p>
     <br>
+    <h5>By <a href="https://github.com/ppmint1" target="_blank">@ppmint1</a>, intended to be used as infrastructure testing.</h5>
+    <p>Source code publicly available here at <a href="https://github.com/ppmint1/laravel-test" target="_blank"> https://github.com/ppmint1/laravel-test</a></p>
+    <br>
+    <p>Server IP : <strong>{{ request()->server('SERVER_ADDR') }}</strong></p>
+    <p>Your IP : <strong>{{ request()->ip() }}</strong><br>Your IP address may be different from the one used by your current device if you are using a VPN, Proxy, behind NAT, or your ISP uses CGNAT (Carrier-grade NAT). </p>
     <br>
     <p>If you see this, then your server and/or infrastructure is working and this Laravel application was successfully deployed. Congrats :)</p>
     <!-- Optional JavaScript; choose one of the two! -->
