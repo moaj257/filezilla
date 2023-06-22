147416ca617bbb5d01dfa81434e02775f74374d5
moaj257 <moaj257@gmail.com>
2023-06-22 17:13:20 +0530

    Update index.jsx

--- resources/js/src/index.jsx
+++ resources/js/src/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
     return (
         <div className="text-3xl font-bold text-white">
-            App
+            Hello World!
         </div>
     );
 }
